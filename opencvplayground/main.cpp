62874eed5069dd22d8582eae79a9d32929a9f8e1
larry <larry@larrys-macbook-air.local>
2016-12-10 12:14:50 -0800

    add opencv

--- opencvplayground/main.cpp
+++ opencvplayground/main.cpp
@@ -1,15 +1,22 @@
-//
-//  main.cpp
-//  opencvplayground
-//
-//  Created by larry on 12/10/16.
-//  Copyright © 2016 larry. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "opencv2/opencv.hpp"
+using namespace cv;
+int main(int, char**)
+{
+    VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+    Mat edges;
+    namedWindow("edges",1);
+    for(;;)
+    {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        cvtColor(frame, edges, COLOR_BGR2GRAY);
+        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        Canny(edges, edges, 0, 30, 3);
+        imshow("edges", edges);
+        if(waitKey(30) >= 0) break;
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
