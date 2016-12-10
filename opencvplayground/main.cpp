d68db789532a3dd35b65b47ae7a7e031a6c1e1fa
larry <larry@larrys-macbook-air.local>
2016-12-10 12:26:05 -0800

    dfdf

--- opencvplayground/main.cpp
+++ opencvplayground/main.cpp
@@ -1,22 +1,87 @@
-#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace cv;
+using namespace std;
+
+Mat src_gray;
+int thresh = 100;
+int max_thresh = 255;
+RNG rng(12345);
+
+void thresh_callback(int, void* );
+int testContours( Mat& src );
+void capturecamera();
+
+
 int main(int, char**)
 {
-    VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
+    Mat src = imread("/Users/larry/sampleimages/lenna.png", 1);
+    testContours(src);
+    return 0;
+}
+
+void capturecamera()
+{
+    VideoCapture cap(0);
+    if(!cap.isOpened())return;
+    
     Mat edges;
     namedWindow("edges",1);
-    for(;;)
-    {
+    for(;;){
         Mat frame;
-        cap >> frame; // get a new frame from camera
+        cap >> frame;
         cvtColor(frame, edges, COLOR_BGR2GRAY);
         GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
         Canny(edges, edges, 0, 30, 3);
         imshow("edges", edges);
         if(waitKey(30) >= 0) break;
     }
-    // the camera will be deinitialized automatically in VideoCapture destructor
-    return 0;
+}
+
+
+int testContours( Mat& src )
+{
+ 
+    cvtColor( src, src_gray, CV_BGR2GRAY );
+    blur( src_gray, src_gray, Size(3,3) );
+    
+    /// Create Window
+    string source_window = "Source";
+    namedWindow( source_window, CV_WINDOW_AUTOSIZE );
+    imshow( source_window, src );
+    
+    createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
+    thresh_callback( 0, 0 );
+    
+    waitKey(0);
+    return(0);
+}
+
+
+void thresh_callback(int, void* )
+{
+    Mat canny_output;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    
+    /// Detect edges using canny
+    Canny( src_gray, canny_output, thresh, thresh*2, 3 );
+    /// Find contours
+    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    
+    /// Draw contours
+    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    for( int i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+    }
+    
+    /// Show in a window
+    namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+    imshow( "Contours", drawing );
 }
